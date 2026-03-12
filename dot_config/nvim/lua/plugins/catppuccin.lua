0003659c0685f3d49930efa24a4684491be79705
mariusrueve <44000950+mariusrueve@users.noreply.github.com>
2026-03-12 15:30:10 +0100

    Add Catppuccin theme support for Neovim and Tmux

---
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "latte",
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
